7829f29906c55231a39cb648044e799dc1711aaa
JB-Gra <98495762+JB-Gra@users.noreply.github.com>
2023-06-13 11:03:20 -0600

    Función de ordenamiento por orden alfabético añadido

--- src/data.js
+++ src/data.js
@@ -1,10 +1,41 @@
-// estas funciones son de ejemplo
-
 import data from './data/ghibli/ghibli.js'
 
 export const ghibliData = () => {
   return data.films;
 };
+
+// ~ Función de filtrado ~
+
+export const sortMovies = {
+  sortOrder: function (orderSelect, movies) {
+    if (orderSelect === "sort-descending") {
+      movies.sort(function (a, b) {
+        if (a.title.replace(/^The /, "") < b.title.replace(/^The /, "")) {
+          return -1;
+        }
+        if (a.title.replace(/^The /, "") > b.title.replace(/^The /, "")) {
+          return 1;
+        }
+        return 0;
+        })
+        return movies;
+      };
+    if (orderSelect === "sort-ascending") {
+      movies.sort(function (a, b) {
+        if (a.title.replace(/^The /, "") > b.title.replace(/^The /, "")) {
+          return -1;
+        }
+        if (a.title.replace(/^The /, "") < b.title.replace(/^The /, "")) {
+          return 1;
+        }
+        return 0;
+        })
+      return movies;
+    }
+  }
+};
+
+// estas funciones son de ejemplo
 
 // export const anotherExample = () => {
 //   return 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
 import { ghibliData } from './data.js';
+import { sortMovies } from './data.js';
 
 // import data from './data/lol/lol.js';
 // import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(ghibliData);
+console.log(sortMovies);
 
 const movieList = ghibliData();
 const movieContainer = document.querySelector('#movie-container');
+const orderTypes = document.querySelector('#movie-filters');
 
-console.log(movieList);
-
-for (let i = 0; i < movieList.length; i++) {
-  const moviePosters = `<img class="movie-poster" src="${movieList[i].poster}" alt="${movieList[i].title} poster" />`
+const showMovies= function() {
+  for (let i = 0; i < movieList.length; i++) {
+  const moviePosters = `<figure class="movie-poster"><img class="poster-img" src="${movieList[i].poster}" alt="${movieList[i].title} poster" /><figcaption class="img-caption">${movieList[i].title}</figcaption></figure>`
   movieContainer.innerHTML += moviePosters;
-}
+  };
+};
+
+showMovies();
+
+orderTypes.addEventListener("change", function() {
+  document.getElementById('movie-container').innerHTML = '';
+  const orderSelect = document.querySelector("#movie-filters").value;
+  const sortSelected = sortMovies.sortOrder(orderSelect, movieList);
+  console.log(sortSelected);
+  showMovies();
+});

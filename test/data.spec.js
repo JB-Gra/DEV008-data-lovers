cd303e3d6deca22a16101ea5f8beaf22a75e5747
JB-Gra <98495762+JB-Gra@users.noreply.github.com>
2023-06-23 11:13:35 -0600

    Algunos tests de ejemplo añadidos

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,53 @@
-import { example, anotherExample } from '../src/data.js';
+// =============================
+// Import of 'data.js' functions
+// =============================
 
+import { ghibliData, filterMovies, sortMovies } from '../src/data';
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+// =============================
+// 'filterMovies' function tests
+// =============================
+
+describe('filterMovies', () => {
+  it('Should be an object', () => {
+    expect(typeof filterMovies).toBe('object');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
+  // ============================================
+  // 'filterMovies.filterDirectors' function test
+  // ============================================
 
+  describe('filterMovies.filterDirectors', () => {
+    it('Should be a function', () => {
+      expect(typeof filterMovies.filterDirectors).toBe('function');
+    });
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    const movies = ghibliData();
+
+    it('Should filter movies directed by Hayao Miyazaki', () => {
+      console.log('checking', movies);
+      // expect(filterMovies.filterDirectors('Hayao Miyazaki', movies)).toBe('Hayao Miyazaki');
+    });
+  })
+
+})
+
+// ===========================
+// 'sortMovies' function tests
+// ===========================
+
+describe('sortMovies', () => {
+  it('Should be an object', () => {
+    expect(typeof sortMovies).toBe('object');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+  // ====================================
+  // 'sortMovies.sortOrder' function test
+  // ====================================
+
+  describe('sortMovies', () => {
+    it('Should be a function', () => {
+      expect(typeof sortMovies.sortOrder).toBe('function');
+    });
+  })
+})
